962f19522255e0b3e43000dc8df7b3bc2c6b025a
lk23286 <lk23286@gmail.com>
2022-07-12 17:10:12 +0200

    clean code + button still doesn't work because UserDefault can't store array with objects. It indicates that it is not a database

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,19 +11,14 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    
-    
     var defaults = UserDefaults.standard
     let key = "storedArray"
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+ 
         let newItem1 = Item()
         newItem1.name = "Find Mike"
-        newItem1.done = true
         itemArray.append(newItem1)
         
         let newItem2 = Item()
@@ -33,7 +28,6 @@ class TodoListViewController: UITableViewController {
         let newItem3 = Item()
         newItem3.name = "Demolish Demogorogon"
         itemArray.append(newItem3)
-        
         
         if let storedArray = defaults.array(forKey: key) as? [Item] {
             itemArray = storedArray
@@ -45,78 +39,44 @@ class TodoListViewController: UITableViewController {
         return itemArray.count
     }
     
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListCell", for: indexPath)
-        
-        cell.textLabel?.text = itemArray[indexPath.row].name
-        
-        if itemArray[indexPath.row].done == true {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }
-       
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       
-        if itemArray[indexPath.row].done == true {
-            itemArray[indexPath.row].done = false
-        } else {
-            itemArray[indexPath.row].done = true
-        }
-        
-    //    itemArray[indexPath.row].done ? false : true
-        
+       itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         tableView.reloadData()
-
         tableView.deselectRow(at: indexPath, animated: true)
-        
-//        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        } else {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        }
-   
-     
-      
     }
+    
 //MARK: Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
-        
-        
         let alert = UIAlertController(title: "Add New Todoey Items", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add Items", style: .default) { (action) in
             // What will happen when the user click on the Add Items button on our UIAlert
             print("Add Item")
-            
             let newItem = Item()
-            
             newItem.name = textField.text!
-            
             self.itemArray.append(newItem)
-            
             self.defaults.set(self.itemArray, forKey: self.key)
-            
             self.tableView.reloadData()
-            
         }
         
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
-           
             print("now")
         }
-        alert.addAction(action)
         
+        alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
